9cbb4eec34f7a75ead1447ec96e139e514c38369
ggservice007 <ggservice007@126.com>
2021-02-03 22:27:23 +0800

    Add the basic files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-happy-bokeh
+
+nteractive Data Visualization in the browser, from Python. Based on [bokeh](https://github.com/bokeh). 
---
+++ my_happy_bokeh/colors/__init__.py
@@ -0,0 +1,10 @@
+''' 
+Provide classes for representing RGB(A) and HSL(A) colors, as well as
+define common named colors.
+'''
+
+#-----------------------------------------------------------------------------
+# Boilerplate
+#-----------------------------------------------------------------------------
+import logging # isort:skip
+log = logging.getLogger(__name__)
